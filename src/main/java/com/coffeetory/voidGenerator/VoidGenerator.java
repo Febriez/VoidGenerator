1c825f40de8c73636cc464e29415a967e3e9cd55
Febriez <febrie@naver.com>
2024-09-28 13:38:27 +0900

    Initial commit

---
+++ src/main/java/com/coffeetory/voidGenerator/VoidGenerator.java
@@ -0,0 +1,58 @@
+package com.coffeetory.voidGenerator;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.generator.BiomeProvider;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.generator.WorldInfo;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Random;
+
+public final class VoidGenerator extends JavaPlugin {
+
+    @Override
+    public @NotNull ChunkGenerator getDefaultWorldGenerator(@NotNull String worldName, @Nullable String id) {
+        return new VoidChunkGenerator();
+    }
+
+    private static class VoidChunkGenerator extends ChunkGenerator {
+        @Override
+        public void generateBedrock(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ, @NotNull ChunkData chunkData) {
+            if (chunkX == 0 && chunkZ == 0) chunkData.setBlock(0, 63, 0, Material.BEDROCK);
+        }
+
+        @Override
+        public boolean canSpawn(@NotNull World world, int x, int z) {
+            return true;
+        }
+
+        @Override
+        public @Nullable Location getFixedSpawnLocation(@NotNull World world, @NotNull Random random) {
+            return new Location(world, 0, 64, 0);
+        }
+
+        @Override
+        public @Nullable BiomeProvider getDefaultBiomeProvider(@NotNull WorldInfo worldInfo) {
+            return new VoidBiomeProvider();
+        }
+    }
+
+    private static class VoidBiomeProvider extends BiomeProvider {
+
+        public @NotNull Biome getBiome(@NotNull WorldInfo worldInfo, int x, int y, int z) {
+            return Biome.PLAINS;
+        }
+
+        public @NotNull List<Biome> getBiomes(@NotNull WorldInfo worldInfo) {
+            return List.of(Biome.PLAINS);
+        }
+    }
+
+
+}
